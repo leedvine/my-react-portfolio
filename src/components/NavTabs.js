7d69936cb1de27a838d437502c83aa198d52d550
Lee Vine <leedvine@gmail.com>
2023-03-03 00:26:09 +0000

    added redirect file

---
+++ src/components/NavTabs.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function NavTabs() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="about"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          About
+        </NavLink>
+        </li>
+      <li className="nav-item">
+        <NavLink
+          to="discover"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Discover
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="search"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Search
+        </NavLink>
+      
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
