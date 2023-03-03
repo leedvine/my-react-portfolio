cfdc57aed1e1b6d9cab9ab6e5fd400fcfd869e1f
Lee Vine <leedvine@gmail.com>
2023-03-03 22:10:25 +0000

    testing hashtag for refresh

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           {/* Define routes using the Route component to render different page components at different paths */}
           {/* Define a default route that will render the Home component */}
-          <Route path="/" element={<Home />} />
+          <Route path="/#/" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="discover" element={<Discover />} />
           {/* Define a route that will have descendant routes */}
